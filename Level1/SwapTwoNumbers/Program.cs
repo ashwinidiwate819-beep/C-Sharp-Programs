8794b15f06929cf01899f9d3ed854e85b5fe7b15
Ashwini Diwate <ashwinidiwate819@gmail.com>
2026-04-18 19:11:48 +0530

    Swap Two Numbers

---
+++ Level1/SwapTwoNumbers/Program.cs
@@ -0,0 +1,28 @@
+﻿// File: SwapTwoNumbers.cs
+using System;
+
+class SwapTwoNumbers
+{
+    static void Main()
+    {
+        Console.Write("Enter First Number  : ");
+        double num1 = Convert.ToDouble(Console.ReadLine());
+
+        Console.Write("Enter Second Number : ");
+        double num2 = Convert.ToDouble(Console.ReadLine());
+
+        Console.WriteLine("\n========== Before Swap ==========");
+        Console.WriteLine($"First Number  : {num1}");
+        Console.WriteLine($"Second Number : {num2}");
+
+        // Swap using temp variable
+        double temp = num1;
+        num1 = num2;
+        num2 = temp;
+
+        Console.WriteLine("\n========== After Swap ===========");
+        Console.WriteLine($"First Number  : {num1}");
+        Console.WriteLine($"Second Number : {num2}");
+        Console.WriteLine("=================================");
+    }
+}

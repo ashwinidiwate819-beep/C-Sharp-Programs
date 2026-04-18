46e39808dbd76a76b7255c8efd8d80110716ee71
Ashwini Diwate <ashwinidiwate819@gmail.com>
2026-04-18 20:53:11 +0530

    CountDigits Numbers

---
+++ Level1/CountDigits/Program.cs
@@ -0,0 +1,28 @@
+﻿
+using System;
+
+class CountDigits
+{
+    static void Main()
+    {
+        int number, count = 0;
+
+        Console.Write("Enter a number: ");
+        number = int.Parse(Console.ReadLine());
+
+        if (number == 0)
+        {
+            count = 1;
+        }
+        else
+        {
+            while (number != 0)
+            {
+                count++;
+                number /= 10;
+            }
+        }
+
+        Console.WriteLine("Number of digits = " + count);
+    }
+}

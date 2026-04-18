f4c3515951f64d04bcdd7b0d91404390387c7972
Ashwini Diwate <ashwinidiwate819@gmail.com>
2026-04-18 18:43:11 +0530

    Check Positive or Negative NUmber

---
+++ Level1/CheckNumber/Program.cs
@@ -0,0 +1,18 @@
+﻿// File: CheckNumber.cs
+using System;
+
+class CheckNumber
+{
+    static void Main()
+    {
+        Console.Write("Enter a number: ");
+        double number = Convert.ToDouble(Console.ReadLine());
+
+        if (number > 0)
+            Console.WriteLine($"{number} is a Positive number.");
+        else if (number < 0)
+            Console.WriteLine($"{number} is a Negative number.");
+        else
+            Console.WriteLine("The number is Zero.");
+    }
+}

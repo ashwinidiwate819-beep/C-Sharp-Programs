49c64a675d1351cb699d8e969cac35757c54e784
Ashwini Diwate <ashwinidiwate819@gmail.com>
2026-04-18 18:50:21 +0530

    calculate simple interest

--- Level1/HelloWorld/obj/Debug/net10.0/HelloWorld.AssemblyInfo.cs
+++ Level1/HelloWorld/obj/Debug/net10.0/HelloWorld.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("HelloWorld")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bbafd1596a4ef4885d9b6d6b2753560a3032eb8f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a672586aab6cab2bc596bd433bc3a874d539e269")]
 [assembly: System.Reflection.AssemblyProductAttribute("HelloWorld")]
 [assembly: System.Reflection.AssemblyTitleAttribute("HelloWorld")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
---
+++ Level1/SimpleInterest/Program.cs
@@ -0,0 +1,28 @@
+﻿// File: SimpleInterest.cs
+using System;
+
+class SimpleInterest
+{
+    static void Main()
+    {
+        Console.Write("Enter Principal Amount: ");
+        double principal = Convert.ToDouble(Console.ReadLine());
+
+        Console.Write("Enter Rate of Interest (% per year): ");
+        double rate = Convert.ToDouble(Console.ReadLine());
+
+        Console.Write("Enter Time (in years): ");
+        double time = Convert.ToDouble(Console.ReadLine());
+
+        double simpleInterest = (principal * rate * time) / 100;
+        double totalAmount = principal + simpleInterest;
+
+        Console.WriteLine("\n========== Result ==========");
+        Console.WriteLine($"Principal Amount : {principal}");
+        Console.WriteLine($"Rate of Interest : {rate}%");
+        Console.WriteLine($"Time             : {time} year(s)");
+        Console.WriteLine($"Simple Interest  : {simpleInterest}");
+        Console.WriteLine($"Total Amount     : {totalAmount}");
+        Console.WriteLine("============================");
+    }
+}

a45c8991d430082046b429e2427c3e76c02e969a
Ashwini Diwate <ashwinidiwate819@gmail.com>
2026-04-18 22:35:33 +0530

    Check Prime Number

---
+++ Level1/CheckPrimeNumber/Program.cs
@@ -0,0 +1,35 @@
+﻿
+using System;
+
+class CheckPrimeNumber
+{
+    static void Main()
+    {
+        int number;
+        bool isPrime = true;
+
+        Console.Write("Enter a number: ");
+        number = int.Parse(Console.ReadLine());
+
+        if (number <= 1)
+        {
+            isPrime = false;
+        }
+        else
+        {
+            for (int i = 2; i <= number / 2; i++)
+            {
+                if (number % i == 0)
+                {
+                    isPrime = false;
+                    break;
+                }
+            }
+        }
+
+        if (isPrime)
+            Console.WriteLine(number + " is a Prime Number.");
+        else
+            Console.WriteLine(number + " is not a Prime Number.");
+    }
+}

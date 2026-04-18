68ec82a39ab2c97c7f350c4530b07cbd70d23ece
Ashwini Diwate <ashwinidiwate819@gmail.com>
2026-04-18 21:02:00 +0530

    PrimeNumbersInRange

---
+++ Level1/PrimeNumbersInRange/Program.cs
@@ -0,0 +1,39 @@
+﻿
+using System;
+
+class PrimeNumbersInRange
+{
+    static void Main()
+    {
+        int start, end;
+        bool isPrime;
+
+        Console.Write("Enter starting number: ");
+        start = int.Parse(Console.ReadLine());
+
+        Console.Write("Enter ending number: ");
+        end = int.Parse(Console.ReadLine());
+
+        Console.WriteLine("Prime numbers between " + start + " and " + end + " are:");
+
+        for (int num = start; num <= end; num++)
+        {
+            if (num <= 1)
+                continue;
+
+            isPrime = true;
+
+            for (int i = 2; i <= num / 2; i++)
+            {
+                if (num % i == 0)
+                {
+                    isPrime = false;
+                    break;
+                }
+            }
+
+            if (isPrime)
+                Console.Write(num + " ");
+        }
+    }
+}

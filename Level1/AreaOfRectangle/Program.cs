b4ce3ef2748e9919470fa670d5cfc80df16a17a2
Ashwini Diwate <ashwinidiwate819@gmail.com>
2026-04-18 22:56:20 +0530

    Area Of Rectangle

---
+++ Level1/AreaOfRectangle/Program.cs
@@ -0,0 +1,20 @@
+﻿
+using System;
+
+class AreaOfRectangle
+{
+    static void Main()
+    {
+        double length, width, area;
+
+        Console.Write("Enter the length of the rectangle: ");
+        length = double.Parse(Console.ReadLine());
+
+        Console.Write("Enter the width of the rectangle: ");
+        width = double.Parse(Console.ReadLine());
+
+        area = length * width;
+
+        Console.WriteLine("Area of the rectangle = " + area);
+    }
+}

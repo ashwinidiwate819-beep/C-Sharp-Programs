5d1292ec0b0d5501a04ef38148755f684e6e75d8
Ashwini Diwate <ashwinidiwate819@gmail.com>
2026-04-18 22:50:07 +0530

    Update

---
+++ Level1/CelsiusToFahrenheit/Program.cs
@@ -0,0 +1,17 @@
+﻿
+using System;
+
+class CelsiusToFahrenheit
+{
+    static void Main()
+    {
+        double celsius, fahrenheit;
+
+        Console.Write("Enter temperature in Celsius: ");
+        celsius = double.Parse(Console.ReadLine());
+
+        fahrenheit = (celsius * 9 / 5) + 32;
+
+        Console.WriteLine("Temperature in Fahrenheit = " + fahrenheit);
+    }
+}

61e094dcf3ce42b83bdc00107f519aae978b3095
Ashwini Diwate <ashwinidiwate819@gmail.com>
2026-04-18 22:52:41 +0530

    AreaOfCircle

---
+++ Level1/AreaOfCircle/Program.cs
@@ -0,0 +1,17 @@
+﻿
+using System;
+
+class AreaOfCircle
+{
+    static void Main()
+    {
+        double radius, area;
+
+        Console.Write("Enter the radius of the circle: ");
+        radius = double.Parse(Console.ReadLine());
+
+        area = Math.PI * radius * radius;
+
+        Console.WriteLine("Area of the circle = " + area);
+    }
+}

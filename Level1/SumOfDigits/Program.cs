15eb5bedada304dfe9751d1d78608e9427cd3998
Ashwini Diwate <ashwinidiwate819@gmail.com>
2026-04-18 20:49:19 +0530

    Sum Of Digits

---
+++ Level1/SumOfDigits/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+class SumOfDigits
+{
+    static void Main()
+    {
+        int number, sum = 0, digit;
+
+        Console.Write("Enter a number: ");
+        number = int.Parse(Console.ReadLine());
+
+        while (number != 0)
+        {
+            digit = number % 10;
+            sum += digit;
+            number /= 10;
+        }
+
+        Console.WriteLine("Sum of digits = " + sum);
+    }
+}

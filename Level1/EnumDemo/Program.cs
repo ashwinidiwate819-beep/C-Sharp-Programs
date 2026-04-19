c24c47492554361833525701856e58fbe65cd96e
Ashwini Diwate <ashwinidiwate819@gmail.com>
2026-04-19 21:31:28 +0530

    Update

---
+++ Level1/EnumDemo/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+enum Days { Sunday, Monday, Tuesday, Wednesday, Thursday, Friday, Saturday }
+
+class EnumDemo
+{
+    static void Main()
+    {
+        Days today = Days.Monday;
+        Console.WriteLine("Today is: " + today);
+    }
+}
+

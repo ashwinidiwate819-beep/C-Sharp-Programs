971ad9e209457ccbd1e61d9b9ef806beea4e7676
Ashwini Diwate <ashwinidiwate819@gmail.com>
2026-04-19 20:29:27 +0530

    Salary calculation

---
+++ Level1/SalaryCalculation/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+class SalaryCalculation
+{
+    static void Main()
+    {
+        double basic, hra, da, totalSalary;
+
+        Console.Write("Enter Basic Salary: ");
+        basic = Convert.ToDouble(Console.ReadLine());
+
+        hra = basic * 0.20;   // 20% HRA
+        da = basic * 0.10;    // 10% DA
+
+        totalSalary = basic + hra + da;
+
+        Console.WriteLine("HRA = " + hra);
+        Console.WriteLine("DA = " + da);
+        Console.WriteLine("Total Salary = " + totalSalary);
+    }
+}

1af25025aee1ddc3eaca5a99680b4014e77abdab
Ashwini Diwate <ashwinidiwate819@gmail.com>
2026-04-19 19:31:32 +0530

    demonstrate constants and readonly

---
+++ Level1/ConstReadonlyDemo/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+class ConstReadonlyDemo
+{
+    // Constant variable
+    const double PI = 3.14159;
+
+    // Readonly variable
+    readonly int rollNo;
+
+    // Constructor
+    ConstReadonlyDemo(int r)
+    {
+        rollNo = r;   // readonly can be assigned in constructor
+    }
+
+    static void Main()
+    {
+        ConstReadonlyDemo obj = new ConstReadonlyDemo(101);
+
+        Console.WriteLine("Constant Value:");
+        Console.WriteLine("PI = " + PI);
+
+        Console.WriteLine();
+
+        Console.WriteLine("Readonly Value:");
+        Console.WriteLine("Roll Number = " + obj.rollNo);
+    }
+}

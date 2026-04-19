4c32fe76d6c69a16e477df792762894316a60728
Ashwini Diwate <ashwinidiwate819@gmail.com>
2026-04-19 20:24:55 +0530

    Power Of Number

---
+++ Level1/PowerOfNumber/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+class PowerOfNumber
+{
+    static void Main()
+    {
+        int baseNum, power, result = 1;
+
+        Console.Write("Enter base number: ");
+        baseNum = Convert.ToInt32(Console.ReadLine());
+
+        Console.Write("Enter power: ");
+        power = Convert.ToInt32(Console.ReadLine());
+
+        for (int i = 1; i <= power; i++)
+        {
+            result *= baseNum;
+        }
+
+        Console.WriteLine("Result = " + result);
+    }
+}

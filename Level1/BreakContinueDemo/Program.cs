14cfc4c4d3933a2c6bc2797016aff06bac9cf801
Ashwini Diwate <ashwinidiwate819@gmail.com>
2026-04-19 20:08:21 +0530

    Break Continue

---
+++ Level1/BreakContinueDemo/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+class BreakContinueDemo
+{
+    static void Main()
+    {
+        Console.WriteLine("Using continue:");
+        for (int i = 1; i <= 5; i++)
+        {
+            if (i == 3)
+                continue;
+
+            Console.WriteLine(i);
+        }
+
+        Console.WriteLine("\nUsing break:");
+        for (int i = 1; i <= 5; i++)
+        {
+            if (i == 4)
+                break;
+
+            Console.WriteLine(i);
+        }
+    }
+}

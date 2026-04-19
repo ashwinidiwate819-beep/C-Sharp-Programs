c091dcc8b561d91463e4a29ea7a09bef57b1daaa
Ashwini Diwate <ashwinidiwate819@gmail.com>
2026-04-19 19:57:00 +0530

    For Loop Demo

---
+++ Level1/ForLoopDemo/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+class ForLoopDemo
+{
+    static void Main()
+    {
+        Console.WriteLine("Numbers from 1 to 5:");
+
+        for (int i = 1; i <= 5; i++)
+        {
+            Console.WriteLine(i);
+        }
+    }
+}

6a40b8031686eafd3d8fc3d2b461989b31336a65
Ashwini Diwate <ashwinidiwate819@gmail.com>
2026-04-19 20:22:18 +0530

    Random Number

---
+++ Level1/RandomNumber/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+class RandomNumber
+{
+    static void Main()
+    {
+        Random rnd = new Random();
+
+        Console.WriteLine("Random numbers between 1 and 100:");
+
+        for (int i = 1; i <= 5; i++)
+        {
+            Console.WriteLine(rnd.Next(1, 101));
+        }
+    }
+}

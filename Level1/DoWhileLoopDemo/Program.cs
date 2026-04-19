4f60d99a0aba254fe957fd79b3c81bd527b2c10d
Ashwini Diwate <ashwinidiwate819@gmail.com>
2026-04-19 20:05:28 +0530

    DoWhile Loop

---
+++ Level1/DoWhileLoopDemo/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+class DoWhileLoopDemo
+{
+    static void Main()
+    {
+        int i = 1;
+
+        Console.WriteLine("Numbers from 1 to 5:");
+
+        do
+        {
+            Console.WriteLine(i);
+            i++;
+        } while (i <= 5);
+    }
+}

b03f68e67072ee122eef905559e9b16fa3ca6338
Ashwini Diwate <ashwinidiwate819@gmail.com>
2026-04-19 20:03:10 +0530

    While Loop

---
+++ Level1/WhileLoopDemo/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+class WhileLoopDemo
+{
+    static void Main()
+    {
+        int i = 1;
+
+        Console.WriteLine("Numbers from 1 to 5:");
+
+        while (i <= 5)
+        {
+            Console.WriteLine(i);
+            i++;
+        }
+    }
+}

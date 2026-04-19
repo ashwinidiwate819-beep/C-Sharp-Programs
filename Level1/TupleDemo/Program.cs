7f808e56e63a0dc926a01227dabf0f7aee050bc8
Ashwini Diwate <ashwinidiwate819@gmail.com>
2026-04-19 21:36:20 +0530

    Update

---
+++ Level1/TupleDemo/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+class TupleDemo
+{
+    static void Main()
+    {
+        var student = (Id: 29, Name: "Karan", Marks: 85);
+
+        Console.WriteLine("ID: " + student.Id);
+        Console.WriteLine("Name: " + student.Name);
+        Console.WriteLine("Marks: " + student.Marks);
+    }
+}

e3a9dbf24c1d16a93e795296afd48d0904d0e54e
Ashwini Diwate <ashwinidiwate819@gmail.com>
2026-04-19 14:22:35 +0530

    ASCII Value

---
+++ Level1/AsciiValue/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+class AsciiValue
+{
+    static void Main()
+    {
+        char ch;
+
+        Console.Write("Enter a character: ");
+        ch = Console.ReadLine()[0];
+
+        int asciiValue = (int)ch;
+
+        Console.WriteLine("ASCII value of '" + ch + "' is: " + asciiValue);
+    }
+}

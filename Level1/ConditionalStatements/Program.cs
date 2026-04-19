25b0be49e11352ba5aa3fa9ae6b3d4a49f26d335
Ashwini Diwate <ashwinidiwate819@gmail.com>
2026-04-19 19:38:17 +0530

    Conditional Statements

---
+++ Level1/ConditionalStatements/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+class ConditionalStatements
+{
+    static void Main()
+    {
+        int num;
+
+        Console.Write("Enter a number: ");
+        num = Convert.ToInt32(Console.ReadLine());
+
+        if (num > 0)
+        {
+            Console.WriteLine("Number is Positive");
+        }
+        else if (num < 0)
+        {
+            Console.WriteLine("Number is Negative");
+        }
+        else
+        {
+            Console.WriteLine("Number is Zero");
+        }
+    }
+}

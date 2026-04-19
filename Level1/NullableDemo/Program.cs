bcaa13309355b4a5ea9b7ef08f60a030240d7915
Ashwini Diwate <ashwinidiwate819@gmail.com>
2026-04-19 21:12:02 +0530

    Update

---
+++ Level1/NullableDemo/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+class NullableDemo
+{
+    static void Main()
+    {
+        int? num = null;
+
+        Console.WriteLine("Nullable value: " + num);
+        Console.WriteLine("Value or default: " + (num ?? 0));
+    }
+}

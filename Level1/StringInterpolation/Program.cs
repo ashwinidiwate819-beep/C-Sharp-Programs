97869ada9f130bf39fb6ccf61ae9d9f7abb8da84
Ashwini Diwate <ashwinidiwate819@gmail.com>
2026-04-19 21:39:20 +0530

    Update

---
+++ Level1/StringInterpolation/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+class StringInterpolation
+{
+    static void Main()
+    {
+        string name = "Ashwini";
+        int age = 20;
+
+        Console.WriteLine($"Name: {name}, Age: {age}");
+    }
+}

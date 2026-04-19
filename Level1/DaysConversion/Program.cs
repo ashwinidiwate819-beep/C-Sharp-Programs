8e2c3636ab601312349196f73052951b5dd53914
Ashwini Diwate <ashwinidiwate819@gmail.com>
2026-04-19 21:09:18 +0530

    Days Conversion

---
+++ Level1/DaysConversion/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+class DaysConversion
+{
+    static void Main()
+    {
+        Console.Write("Enter total days: ");
+        int days = Convert.ToInt32(Console.ReadLine());
+
+        int years = days / 365;
+        days %= 365;
+        int months = days / 30;
+        days %= 30;
+
+        Console.WriteLine("Years: " + years);
+        Console.WriteLine("Months: " + months);
+        Console.WriteLine("Days: " + days);
+    }
+}

fbeb7bedb262397d6fd9ce709b0c42864e697520
Ashwini Diwate <ashwinidiwate819@gmail.com>
2026-04-19 14:27:48 +0530

    Typecasting

---
+++ Level1/TypeCastingDemo/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+class TypeCastingDemo
+{
+    static void Main()
+    {
+        // Implicit Type Casting (Smaller → Larger)
+        int intValue = 50;
+        double doubleValue = intValue;
+
+        Console.WriteLine("Implicit Type Casting:");
+        Console.WriteLine("Int value: " + intValue);
+        Console.WriteLine("Double value: " + doubleValue);
+
+        Console.WriteLine();
+
+        // Explicit Type Casting (Larger → Smaller)
+        double num = 45.75;
+        int result = (int)num;
+
+        Console.WriteLine("Explicit Type Casting:");
+        Console.WriteLine("Double value: " + num);
+        Console.WriteLine("Int value after casting: " + result);
+    }
+}

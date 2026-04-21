e28877018c99a318a71d369e22e91cf4301baf27
Ashwini Diwate <ashwinidiwate819@gmail.com>
2026-04-21 21:42:50 +0530

    Count Words

---
+++ Level2/CountWords/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+class CountWords
+{
+    static void Main()
+    {
+        Console.Write("Enter a sentence: ");
+        string str = Console.ReadLine();
+
+        string[] words = str.Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries);
+        Console.WriteLine("Number of words: " + words.Length);
+    }
+}

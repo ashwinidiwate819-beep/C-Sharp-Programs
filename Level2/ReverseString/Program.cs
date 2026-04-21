2b0a50f6426feee902cf471efd9fb930610ac6c8
Ashwini Diwate <ashwinidiwate819@gmail.com>
2026-04-21 18:48:03 +0530

    ReverseString

---
+++ Level2/ReverseString/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+class ReverseString
+{
+    static void Main()
+    {
+        Console.Write("Enter a string: ");
+        string str = Console.ReadLine();
+
+        char[] arr = str.ToCharArray();
+        Array.Reverse(arr);
+
+        Console.WriteLine("Reversed string: " + new string(arr));
+    }
+}

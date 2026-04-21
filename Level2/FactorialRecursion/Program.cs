9bb0e18f78edd4a795edfc4f25b0945ea713a270
Ashwini Diwate <ashwinidiwate819@gmail.com>
2026-04-21 22:48:34 +0530

    FactorialRecursion

---
+++ Level2/FactorialRecursion/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+class FactorialRecursion
+{
+    static int Factorial(int n)
+    {
+        if (n == 0)
+            return 1;
+
+        return n * Factorial(n - 1);
+    }
+
+    static void Main()
+    {
+        Console.Write("Enter a number: ");
+        int num = Convert.ToInt32(Console.ReadLine());
+
+        Console.WriteLine("Factorial = " + Factorial(num));
+    }
+}

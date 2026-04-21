3aee83ce7cb1a52d34eb824dbe1d1ea74f9bf76d
Ashwini Diwate <ashwinidiwate819@gmail.com>
2026-04-21 22:43:47 +0530

    FibonacciRecursion

---
+++ Level2/FibonacciRecursion/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+class FibonacciRecursion
+{
+    static int Fibonacci(int n)
+    {
+        if (n == 0)
+            return 0;
+        if (n == 1)
+            return 1;
+
+        return Fibonacci(n - 1) + Fibonacci(n - 2);
+    }
+
+    static void Main()
+    {
+        Console.Write("Enter number of terms: ");
+        int terms = Convert.ToInt32(Console.ReadLine());
+
+        Console.WriteLine("Fibonacci Series:");
+        for (int i = 0; i < terms; i++)
+        {
+            Console.Write(Fibonacci(i) + " ");
+        }
+    }
+}

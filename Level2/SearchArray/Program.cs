569e159f8834b9734cce350f82c96c6dd7fa456a
Ashwini Diwate <ashwinidiwate819@gmail.com>
2026-04-21 18:06:28 +0530

    Search Array

---
+++ Level2/SearchArray/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+class SearchArray
+{
+    static void Main()
+    {
+        Console.Write("Enter number of elements: ");
+        int n = Convert.ToInt32(Console.ReadLine());
+
+        int[] arr = new int[n];
+
+        Console.WriteLine("Enter array elements:");
+        for (int i = 0; i < n; i++)
+        {
+            arr[i] = Convert.ToInt32(Console.ReadLine());
+        }
+
+        Console.Write("Enter element to search: ");
+        int search = Convert.ToInt32(Console.ReadLine());
+
+        bool found = false;
+
+        for (int i = 0; i < n; i++)
+        {
+            if (arr[i] == search)
+            {
+                Console.WriteLine("Element found at position: " + (i + 1));
+                found = true;
+                break;
+            }
+        }
+
+        if (!found)
+        {
+            Console.WriteLine("Element not found.");
+        }
+    }
+}

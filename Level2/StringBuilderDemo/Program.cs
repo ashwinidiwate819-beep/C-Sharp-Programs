6d92d4c2d551093e459167cf8597f87e07eb12ba
Ashwini Diwate <ashwinidiwate819@gmail.com>
2026-04-21 22:07:21 +0530

    String Builder Demo

--- Level2/StringBuilderDemo/Program.cs
+++ Level2/StringBuilderDemo/Program.cs
@@ -1,2 +1,25 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.Text;
+
+class StringBuilderDemo
+{
+    static void Main()
+    {
+        StringBuilder sb = new StringBuilder();
+
+        sb.Append("Hello");
+        sb.Append(" ");
+        sb.Append("World");
+
+        Console.WriteLine("After Append: " + sb);
+
+        sb.Insert(5, " C#");
+        Console.WriteLine("After Insert: " + sb);
+
+        sb.Replace("World", "Programming");
+        Console.WriteLine("After Replace: " + sb);
+
+        sb.Remove(5, 3);
+        Console.WriteLine("After Remove: " + sb);
+    }
+}
--- Level2/StringBuilderDemo/obj/Debug/net10.0/StringBuilderDemo.AssemblyInfo.cs
+++ Level2/StringBuilderDemo/obj/Debug/net10.0/StringBuilderDemo.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("StringBuilderDemo")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ebd57bf5cef60dd6e9a6ef3f5788efdeba53ed76")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+74bf08d092e74c9205850a559aaebca3abed07a1")]
 [assembly: System.Reflection.AssemblyProductAttribute("StringBuilderDemo")]
 [assembly: System.Reflection.AssemblyTitleAttribute("StringBuilderDemo")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]

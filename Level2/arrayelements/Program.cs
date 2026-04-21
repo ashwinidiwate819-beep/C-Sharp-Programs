2b00fff825e1610f5877dc8058245048c20abb33
Ashwini Diwate <ashwinidiwate819@gmail.com>
2026-04-21 13:44:44 +0530

    update

---
+++ Level2/arrayelements/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+class ReadDisplayArray
+{
+    static void Main()
+    {
+        Console.Write("Enter number of elements: ");
+        int n = int.Parse(Console.ReadLine());
+
+        int[] arr = new int[n];
+
+        Console.WriteLine("Enter array elements:");
+        for (int i = 0; i < n; i++)
+        {
+            arr[i] = int.Parse(Console.ReadLine());
+        }
+
+        Console.WriteLine("Array elements are:");
+        foreach (int item in arr)
+        {
+            Console.Write(item + " ");
+        }
+    }
+}

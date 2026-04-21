bb693eec7655dbdf69479e795cc0854f58d11a52
Ashwini Diwate <ashwinidiwate819@gmail.com>
2026-04-21 18:39:17 +0530

    Matrix Transpose

---
+++ Level2/MatrixTranspose/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+class MatrixTranspose
+{
+    static void Main()
+    {
+        Console.Write("Enter number of rows: ");
+        int rows = Convert.ToInt32(Console.ReadLine());
+
+        Console.Write("Enter number of columns: ");
+        int cols = Convert.ToInt32(Console.ReadLine());
+
+        int[,] matrix = new int[rows, cols];
+        int[,] transpose = new int[cols, rows];
+
+        Console.WriteLine("Enter matrix elements:");
+        for (int i = 0; i < rows; i++)
+        {
+            for (int j = 0; j < cols; j++)
+            {
+                matrix[i, j] = Convert.ToInt32(Console.ReadLine());
+            }
+        }
+
+        for (int i = 0; i < rows; i++)
+        {
+            for (int j = 0; j < cols; j++)
+            {
+                transpose[j, i] = matrix[i, j];
+            }
+        }
+
+        Console.WriteLine("Transpose of matrix:");
+        for (int i = 0; i < cols; i++)
+        {
+            for (int j = 0; j < rows; j++)
+            {
+                Console.Write(transpose[i, j] + " ");
+            }
+            Console.WriteLine();
+        }
+    }
+}

8bdd5d9ba499c41aff77dba2020941527b5048c4
Ashwini Diwate <ashwinidiwate819@gmail.com>
2026-04-21 17:52:36 +0530

    LargestSmallest

---
+++ Level2/LargestSmallestArray/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+class LargestSmallestArray
+{
+    static void Main()
+    {
+        int[] arr = { 25, 10, 45, 5, 30 };
+
+        int largest = arr[0];
+        int smallest = arr[0];
+
+        foreach (int num in arr)
+        {
+            if (num > largest)
+                largest = num;
+
+            if (num < smallest)
+                smallest = num;
+        }
+
+        Console.WriteLine("Largest = " + largest);
+        Console.WriteLine("Smallest = " + smallest);
+    }
+}

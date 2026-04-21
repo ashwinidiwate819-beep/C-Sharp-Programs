a3285a5293ad48d66989479cccea9586dd75d435
Ashwini Diwate <ashwinidiwate819@gmail.com>
2026-04-21 18:02:04 +0530

    Remove Duplicates element

---
+++ Level2/RemoveDuplicates/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Linq;
+
+class RemoveDuplicates
+{
+    static void Main()
+    {
+        int[] arr = { 1, 2, 2, 3, 4, 4, 5 };
+
+        int[] uniqueArr = arr.Distinct().ToArray();
+
+        Console.WriteLine("Array after removing duplicates:");
+        foreach (int num in uniqueArr)
+        {
+            Console.Write(num + " ");
+        }
+    }
+}
+ 

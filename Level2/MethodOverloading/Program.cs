4d226b73351f8f4cf3e9feaa31f3b38aa28a92e9
Ashwini Diwate <ashwinidiwate819@gmail.com>
2026-04-21 22:34:23 +0530

    MethodOverloading

---
+++ Level2/MethodOverloading/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+class MethodOverloading
+{
+    static int Add(int a, int b)
+    {
+        return a + b;
+    }
+
+    static double Add(double a, double b)
+    {
+        return a + b;
+    }
+
+    static int Add(int a, int b, int c)
+    {
+        return a + b + c;
+    }
+
+    static void Main()
+    {
+        Console.WriteLine("Add(int, int): " + Add(10, 20));
+        Console.WriteLine("Add(double, double): " + Add(10.5, 20.5));
+        Console.WriteLine("Add(int, int, int): " + Add(5, 10, 15));
+    }
+}

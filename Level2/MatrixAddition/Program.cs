499bdf06239e1ba0bb49fa137a29220c8bda4960
Ashwini Diwate <ashwinidiwate819@gmail.com>
2026-04-21 18:10:25 +0530

    Matrix Addition

---
+++ Level2/MatrixAddition/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+
+class MatrixAddition
+{
+    static void Main()
+    {
+        Console.Write("Enter number of rows: ");
+        int rows = Convert.ToInt32(Console.ReadLine());
+
+        Console.Write("Enter number of columns: ");
+        int cols = Convert.ToInt32(Console.ReadLine());
+
+        int[,] matrix1 = new int[rows, cols];
+        int[,] matrix2 = new int[rows, cols];
+        int[,] sum = new int[rows, cols];
+
+        Console.WriteLine("Enter elements of Matrix 1:");
+        for (int i = 0; i < rows; i++)
+        {
+            for (int j = 0; j < cols; j++)
+            {
+                matrix1[i, j] = Convert.ToInt32(Console.ReadLine());
+            }
+        }
+
+        Console.WriteLine("Enter elements of Matrix 2:");
+        for (int i = 0; i < rows; i++)
+        {
+            for (int j = 0; j < cols; j++)
+            {
+                matrix2[i, j] = Convert.ToInt32(Console.ReadLine());
+            }
+        }
+
+        for (int i = 0; i < rows; i++)
+        {
+            for (int j = 0; j < cols; j++)
+            {
+                sum[i, j] = matrix1[i, j] + matrix2[i, j];
+            }
+        }
+
+        Console.WriteLine("Sum of matrices:");
+        for (int i = 0; i < rows; i++)
+        {
+            for (int j = 0; j < cols; j++)
+            {
+                Console.Write(sum[i, j] + " ");
+            }
+            Console.WriteLine();
+        }
+    }
+}

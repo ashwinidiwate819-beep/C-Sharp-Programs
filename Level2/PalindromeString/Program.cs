12e2d5d46b5ee58d4a28d00761b5bdaf59fba1d2
Ashwini Diwate <ashwinidiwate819@gmail.com>
2026-04-21 18:53:15 +0530

    Palindrom String

---
+++ Level2/PalindromeString/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+class PalindromeString
+{
+    static void Main()
+    {
+        Console.Write("Enter a string: ");
+        string str = Console.ReadLine();
+
+        string reverse = "";
+        for (int i = str.Length - 1; i >= 0; i--)
+        {
+            reverse += str[i];
+        }
+
+        if (str.Equals(reverse, StringComparison.OrdinalIgnoreCase))
+            Console.WriteLine("String is a palindrome.");
+        else
+            Console.WriteLine("String is not a palindrome.");
+    }
+}
+

97f79aeb966cddfe8da67f08e9116fc28432f0c3
Ashwini Diwate <ashwinidiwate819@gmail.com>
2026-04-21 21:46:25 +0530

    Character Frequency

---
+++ Level2/CharacterFrequency/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+
+class CharacterFrequency
+{
+    static void Main()
+    {
+        Console.Write("Enter a string: ");
+        string str = Console.ReadLine();
+
+        Dictionary<char, int> freq = new Dictionary<char, int>();
+
+        foreach (char ch in str)
+        {
+            if (ch != ' ')
+            {
+                if (freq.ContainsKey(ch))
+                    freq[ch]++;
+                else
+                    freq[ch] = 1;
+            }
+        }
+
+        Console.WriteLine("Character Frequency:");
+        foreach (var item in freq)
+        {
+            Console.WriteLine(item.Key + " : " + item.Value);
+        }
+    }
+}
+

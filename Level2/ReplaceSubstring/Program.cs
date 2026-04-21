ebd57bf5cef60dd6e9a6ef3f5788efdeba53ed76
Ashwini Diwate <ashwinidiwate819@gmail.com>
2026-04-21 21:55:30 +0530

    Replace Substring

---
+++ Level2/ReplaceSubstring/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+class ReplaceSubstring
+{
+    static void Main()
+    {
+        Console.Write("Enter the main string: ");
+        string mainStr = Console.ReadLine();
+
+        Console.Write("Enter substring to replace: ");
+        string oldSub = Console.ReadLine();
+
+        Console.Write("Enter new substring: ");
+        string newSub = Console.ReadLine();
+
+        string result = mainStr.Replace(oldSub, newSub);
+
+        Console.WriteLine("Updated string: " + result);
+    }
+}

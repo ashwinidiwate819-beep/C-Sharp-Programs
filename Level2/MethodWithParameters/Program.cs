e1a1206997024ff92d7b3318df6c684e0e988246
Ashwini Diwate <ashwinidiwate819@gmail.com>
2026-04-21 22:27:46 +0530

    MethowithParameters

---
+++ Level2/MethodWithParameters/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+class MethodWithParameters
+{
+    static void Add(int a, int b)
+    {
+        int sum = a + b;
+        Console.WriteLine("Sum = " + sum);
+    }
+
+    static void Main()
+    {
+        Console.Write("Enter first number: ");
+        int x = Convert.ToInt32(Console.ReadLine());
+
+        Console.Write("Enter second number: ");
+        int y = Convert.ToInt32(Console.ReadLine());
+
+        Add(x, y);
+    }
+}

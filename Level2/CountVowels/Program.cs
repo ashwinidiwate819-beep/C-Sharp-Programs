807f362464fa408006c2b3e1988cbf0830b411ea
Ashwini Diwate <ashwinidiwate819@gmail.com>
2026-04-21 18:43:27 +0530

    Count Vowels

---
+++ Level2/CountVowels/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+class CountVowels
+{
+    static void Main()
+    {
+        Console.Write("Enter a string: ");
+        string str = Console.ReadLine().ToLower();
+
+        int count = 0;
+        foreach (char ch in str)
+        {
+            if ("aeiou".Contains(ch))
+            {
+                count++;
+            }
+        }
+
+        Console.WriteLine("Number of vowels: " + count);
+    }
+}
+

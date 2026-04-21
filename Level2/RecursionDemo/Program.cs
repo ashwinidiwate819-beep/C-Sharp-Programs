d59ad9b728df18314e64797ec71cf29e5e2557ce
Ashwini Diwate <ashwinidiwate819@gmail.com>
2026-04-21 22:39:35 +0530

    RecursionDemo

---
+++ Level2/RecursionDemo/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+class RecursionDemo
+{
+    static void PrintNumbers(int n)
+    {
+        if (n == 0)
+            return;
+
+        Console.WriteLine(n);
+        PrintNumbers(n - 1);
+    }
+
+    static void Main()
+    {
+        Console.Write("Enter a number: ");
+        int num = Convert.ToInt32(Console.ReadLine());
+
+        PrintNumbers(num);
+    }
+}

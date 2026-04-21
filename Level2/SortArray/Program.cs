0a9152cee17c862776ea2ef476c0906586bab7e5
Ashwini Diwate <ashwinidiwate819@gmail.com>
2026-04-21 17:55:10 +0530

    Sort Array

---
+++ Level2/SortArray/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+class SortArray
+{
+    static void Main()
+    {
+        int[] arr = { 40, 10, 30, 20, 50 };
+
+        Array.Sort(arr);
+
+        Console.WriteLine("Sorted Array:");
+        foreach (int num in arr)
+        {
+            Console.Write(num + " ");
+        }
+    }
+}

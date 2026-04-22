4935e574ec35ad2eb43dc45b19fa30eb9096fb29
Ashwini Diwate <ashwinidiwate819@gmail.com>
2026-04-22 21:54:10 +0530

    Static Methods

---
+++ Level2/StaticMethod/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+class MathOperations
+{
+    public static void Square(int num)
+    {
+        Console.WriteLine("Square = " + (num * num));
+    }
+}
+
+class StaticMethodUserInput
+{
+    static void Main()
+    {
+        Console.Write("Enter a number: ");
+        int num = int.Parse(Console.ReadLine());
+
+        MathOperations.Square(num); // static method call
+    }
+}

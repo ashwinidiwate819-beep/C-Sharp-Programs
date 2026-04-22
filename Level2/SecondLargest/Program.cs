0f2d94985ff75359d6811c96c14cb7ba0f364949
Ashwini Diwate <ashwinidiwate819@gmail.com>
2026-04-22 22:01:36 +0530

    Second Largest Number

---
+++ Level2/SecondLargest/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+class SecondLargest
+{
+    static void Main()
+    {
+        Console.Write("Enter array size: ");
+        int n = int.Parse(Console.ReadLine());
+
+        int[] arr = new int[n];
+
+        for (int i = 0; i < n; i++)
+        {
+            Console.Write("Enter element " + (i + 1) + ": ");
+            arr[i] = int.Parse(Console.ReadLine());
+        }
+
+        Array.Sort(arr);
+
+        Console.WriteLine("Second Largest Element = " + arr[n - 2]);
+    }
+}

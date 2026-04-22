109dd43ff0bbf9f60dfa0f573d4ffd59f3487145
Ashwini Diwate <ashwinidiwate819@gmail.com>
2026-04-22 21:51:29 +0530

    Named Argument

---
+++ Level2/NamedArguments/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+class NamedArgumentsUserInput
+{
+    static void StudentDetails(string name, int rollNo, string course)
+    {
+        Console.WriteLine("Name: " + name);
+        Console.WriteLine("Roll No: " + rollNo);
+        Console.WriteLine("Course: " + course);
+    }
+
+    static void Main()
+    {
+        Console.Write("Enter name: ");
+        string name = Console.ReadLine();
+
+        Console.Write("Enter roll number: ");
+        int roll = int.Parse(Console.ReadLine());
+
+        Console.Write("Enter course: ");
+        string course = Console.ReadLine();
+
+        // Named arguments (order changed)
+        StudentDetails(course: course, rollNo: roll, name: name);
+    }
+}

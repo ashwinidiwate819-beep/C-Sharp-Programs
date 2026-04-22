7e47aa243167222d5d2443e8019503382a1f3924
Ashwini Diwate <ashwinidiwate819@gmail.com>
2026-04-22 21:57:41 +0530

    Array To method

---
+++ Level2/ArrayToMethod/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+class ArrayToMethod
+{
+    static void DisplayArray(int[] arr)
+    {
+        Console.WriteLine("Array elements:");
+        foreach (int item in arr)
+        {
+            Console.Write(item + " ");
+        }
+    }
+
+    static void Main()
+    {
+        Console.Write("Enter array size: ");
+        int size = int.Parse(Console.ReadLine());
+
+        int[] arr = new int[size];
+
+        for (int i = 0; i < size; i++)
+        {
+            Console.Write("Enter element " + (i + 1) + ": ");
+            arr[i] = int.Parse(Console.ReadLine());
+        }
+
+        DisplayArray(arr);
+    }
+}

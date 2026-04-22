83f5a740a9d3b5ff7c46e9f747ebb3e52532c353
Ashwini Diwate <ashwinidiwate819@gmail.com>
2026-04-22 18:23:47 +0530

    Out Refout and ref keywords

---
+++ Level2/OutRefDemo/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+class OutRefUserInput
+{
+    static void Add(ref int a)
+    {
+        a = a + 10;
+    }
+
+    static void Multiply(out int b)
+    {
+        b = 0;
+        Console.Write("Enter number for out parameter: ");
+        b = int.Parse(Console.ReadLine());
+        b = b * 2;
+    }
+
+    static void Main()
+    {
+        Console.Write("Enter number for ref parameter: ");
+        int x = int.Parse(Console.ReadLine());
+
+        Add(ref x);
+        Console.WriteLine("After ref: " + x);
+
+        int y;
+        Multiply(out y);
+        Console.WriteLine("After out: " + y);
+    }
+}

d5b4537e553e8e33b2f70d1138afea6eeecebe73
Ashwini Diwate <ashwinidiwate819@gmail.com>
2026-04-22 21:45:54 +0530

    update

---
+++ Level2/ParamsDemo/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+class ParamsUserInput
+{
+    static void AddNumbers(params int[] numbers)
+    {
+        int sum = 0;
+        foreach (int n in numbers)
+        {
+            sum += n;
+        }
+        Console.WriteLine("Sum = " + sum);
+    }
+
+    static void Main()
+    {
+        Console.Write("Enter how many numbers: ");
+        int count = int.Parse(Console.ReadLine());
+
+        int[] nums = new int[count];
+
+        for (int i = 0; i < count; i++)
+        {
+            Console.Write("Enter number " + (i + 1) + ": ");
+            nums[i] = int.Parse(Console.ReadLine());
+        }
+
+        AddNumbers(nums);
+    }
+}

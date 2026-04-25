1aa6075fbe0288a7b1fdaaa0f49265a599a14eff
Ashwini Diwate <ashwinidiwate819@gmail.com>
2026-04-25 22:19:24 +0530

    Vali Date Email

---
+++ Level2/ValidateEmail/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Enter email address: ");
+        string email = Console.ReadLine();
+
+        string pattern = @"^[^@\s]+@[^@\s]+\.[^@\s]+$";
+
+        if (Regex.IsMatch(email, pattern))
+            Console.WriteLine("Valid Email Address");
+        else
+            Console.WriteLine("Invalid Email Address");
+    }
+}
+

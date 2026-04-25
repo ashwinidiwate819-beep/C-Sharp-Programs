5830bccebc7c1d3c20c3c803774b24dc7a91caa6
Ashwini Diwate <ashwinidiwate819@gmail.com>
2026-04-25 23:19:29 +0530

    Duplicate Elements Array

---
+++ Level2/DuplicateElementsArray/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Linq;
+
+class Program
+{
+    static void Main()
+    {
+        int[] arr = { 1, 2, 3, 2, 4, 5, 3 };
+
+        var duplicates = arr.GroupBy(x => x)
+                            .Where(g => g.Count() > 1)
+                            .Select(g => g.Key);
+
+        Console.WriteLine("Duplicate elements:");
+        foreach (int item in duplicates)
+        {
+            Console.Write(item + " ");
+        }
+    }
+}
+

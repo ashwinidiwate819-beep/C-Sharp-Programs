3d0f762145ebfe265c49bb04c00155f51e30133e
Ashwini Diwate <ashwinidiwate819@gmail.com>
2026-04-25 22:56:42 +0530

    String Formatting Demo

---
+++ Level2/StringFormattingDemo/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        string name = "Ashwini";
+        int age = 21;
+        double marks = 89.456;
+
+        Console.WriteLine("Name: {0}, Age: {1}, Marks: {2:F2}", name, age, marks);
+        Console.WriteLine($"Name: {name}, Age: {age}, Marks: {marks:F2}");
+    }
+}

78cce6b0568c56b8843b4a82348c0c5c9304f2a5
Ashwini Diwate <ashwinidiwate819@gmail.com>
2026-04-25 23:12:43 +0530

    Common Elements Arrays

---
+++ Level2/CommonElementsArray/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Linq;
+
+class Program
+{
+    static void Main()
+    {
+        int[] arr1 = { 1, 2, 3, 4, 5 };
+        int[] arr2 = { 3, 4, 5, 6, 7 };
+
+        var common = arr1.Intersect(arr2);
+
+        Console.WriteLine("Common elements:");
+        foreach (int item in common)
+        {
+            Console.Write(item + " ");
+        }
+    }
+}
+

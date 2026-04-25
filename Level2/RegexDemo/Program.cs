1e4485c51c06b3dc48762654db04d22b99133969
Ashwini Diwate <ashwinidiwate819@gmail.com>
2026-04-25 22:38:56 +0530

    Regex Demo

---
+++ Level2/RegexDemo/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+class Program
+{
+    static void Main()
+    {
+        string text = "My mobile number is 9876543210";
+
+        string pattern = @"\d{10}";
+
+        Match match = Regex.Match(text, pattern);
+
+        if (match.Success)
+            Console.WriteLine("Mobile number found: " + match.Value);
+        else
+            Console.WriteLine("No mobile number found");
+    }
+}
+

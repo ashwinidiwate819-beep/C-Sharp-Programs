e6e327817b8f4a0cf3955f1782570be294d6d690
Ashwini Diwate <ashwinidiwate819@gmail.com>
2026-04-25 22:16:29 +0530

    String Upper Lower

---
+++ Level2/StringUpperLower/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Enter a string: ");
+        string input = Console.ReadLine();
+
+        Console.WriteLine("Uppercase: " + input.ToUpper());
+        Console.WriteLine("Lowercase: " + input.ToLower());
+    }
+}
+

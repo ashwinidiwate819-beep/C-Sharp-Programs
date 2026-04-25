f70c669197ccb7955562af97b1d9abae337c895f
Ashwini Diwate <ashwinidiwate819@gmail.com>
2026-04-25 22:53:50 +0530

    MultiDimensional Arrays Demo

---
+++ Level2/MultiDimensionalArrayDemo/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        int[,] matrix = new int[2, 3]
+        {
+            { 1, 2, 3 },
+            { 4, 5, 6 }
+        };
+
+        for (int i = 0; i < matrix.GetLength(0); i++)
+        {
+            for (int j = 0; j < matrix.GetLength(1); j++)
+            {
+                Console.Write(matrix[i, j] + " ");
+            }
+            Console.WriteLine();
+        }
+    }
+}
+

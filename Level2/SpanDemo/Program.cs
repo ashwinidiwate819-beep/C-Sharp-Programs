03e1aacb2705270725d4f0e73dd1dd75fbb1ce84
Ashwini Diwate <ashwinidiwate819@gmail.com>
2026-04-25 23:23:06 +0530

    Span Demo

---
+++ Level2/SpanDemo/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        int[] numbers = { 10, 20, 30, 40, 50 };
+
+        Span<int> span = numbers;
+        ReadOnlySpan<int> readOnlySpan = numbers;
+
+        span[0] = 100; // Modifies original array
+
+        Console.WriteLine("Span values:");
+        foreach (var item in span)
+            Console.Write(item + " ");
+
+        Console.WriteLine("\nReadOnlySpan values:");
+        foreach (var item in readOnlySpan)
+            Console.Write(item + " ");
+    }
+}
+

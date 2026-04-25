55b1cbf2ca3784eb579f782c69b6099f667b8a74
Ashwini Diwate <ashwinidiwate819@gmail.com>
2026-04-25 22:42:22 +0530

    Indexer Demo

---
+++ Level2/IndexerDemo/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+class Student
+{
+    private string[] names = new string[3];
+
+    // Indexer
+    public string this[int index]
+    {
+        get { return names[index]; }
+        set { names[index] = value; }
+    }
+}
+
+class Program
+{
+    static void Main()
+    {
+        Student s = new Student();
+
+        s[0] = "Ashwini";
+        s[1] = "Rahul";
+        s[2] = "Sneha";
+
+        Console.WriteLine("Student Names:");
+        for (int i = 0; i < 3; i++)
+        {
+            Console.WriteLine(s[i]);
+        }
+    }
+}
+
